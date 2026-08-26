1d032728881a23d1e9f3e9a940e4c38e08e48c89
Rootodus <rootodus@gmail.com>
2026-08-26 13:14:56 +0200

    add dhat dep and update main rs

--- crates/hfm-player/src/main.rs
+++ crates/hfm-player/src/main.rs
@@ -8,13 +8,26 @@ mod pts_offset;
 mod renderer;
 
 use app::App;
-use mimalloc::MiMalloc;
 use winit::event_loop::EventLoop;
 
+#[cfg(feature = "dhat")]
+use dhat::{Alloc, Profiler};
+
+#[cfg(feature = "dhat")]
 #[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+static ALLOC: Alloc = Alloc;
+
+#[cfg(not(feature = "dhat"))]
+use mimalloc::MiMalloc;
+
+#[cfg(not(feature = "dhat"))]
+#[global_allocator]
+static ALLOC: MiMalloc = MiMalloc;
 
 fn main() {
+    #[cfg(feature = "dhat")]
+    let _dhat = Profiler::new_heap();
+
     let event_loop = EventLoop::new().unwrap();
     let mut app = App::new();
     event_loop.run_app(&mut app).unwrap();
